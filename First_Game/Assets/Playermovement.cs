b54106ab4be675d4de5a21dd1fbd40ed32b32ea3
bruhMoment-exe <stefanoshajipetri05@gmail.com>
2020-06-08 22:00:08 +0300

    Add movement

---
+++ First_Game/Assets/Playermovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z ;
+        controller.Move(move*speed*Time.deltaTime);
+    }
+}
