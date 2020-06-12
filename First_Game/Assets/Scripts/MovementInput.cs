5a24ba64d03ee87734dc247af291b58cc7462d5a
MahfoudM <m.mahfoud707@gmail.com>
2020-06-12 22:45:56 +0300

    Tried Again to fix something

---
+++ First_Game/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,105 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviourPunCallbacks
+{
+    public float InputX;
+    public float InputZ;
+    public Vector3 desiredMoveDirection;
+    public bool blockRotationPlayer;
+    public float desiredRotationSpeed;
+    //public Animator anim;
+    public float Speed;
+    public float allowPlayerRotation;
+    public Camera cam;
+    public CharacterController controller;
+    public bool isGrounded;
+    private float verticalVel;
+    private Vector3 moveVector;
+    public float gravity = -9;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundCheckDistance = 0.4f;
+    public LayerMask groundMask;
+    public float JumpHeight = 3f;
+
+    private void Start()
+    {
+        //anim = this.GetComponent<Animator>();
+        cam = Camera.main;
+        controller = this.GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+
+        if (photonView.IsMine)
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+
+            InputMagnitude();
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+    void PlayerMoveAndRotation()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        var camera = Camera.main;
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * InputZ + right * InputX;
+
+        if(blockRotationPlayer == false)
+        {
+            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
+        }
+    }
+
+    void InputMagnitude()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        //anim.SetFloat("InputZ", InputZ, 0.0f, Time.deltaTime * 2f);
+        //anim.SetFloat("InputX", InputX, 0.0f, Time.deltaTime * 2f);
+
+        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+        if(Speed > allowPlayerRotation)
+        {
+          //  anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+            PlayerMoveAndRotation();
+        }
+        else if (Speed < allowPlayerRotation)
+        {
+          //  anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+        }
+    }
+}
--- First_Game/Assets/Scripts/PlayerSpawner.cs
+++ First_Game/Assets/Scripts/PlayerSpawner.cs
@@ -4,16 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpawner : NetworkBehaviour
+public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject playerPrefab = null;
     [SerializeField] private CinemachineFreeLook playerCamera = null;
 
+    public Vector3 spawnPos;
+
+
     void Start()
     {
-        var player = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+        var player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPos, Quaternion.identity);
         playerCamera.Follow = player.transform;
         playerCamera.LookAt = player.transform;
     }
-
 }
