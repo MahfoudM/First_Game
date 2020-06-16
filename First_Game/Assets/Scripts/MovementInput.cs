0c6bd7f0fe2696a3ca507eba5107e57c5dd09b56
MahfoudM <m.mahfoud707@gmail.com>
2020-06-16 14:41:14 +0300

    Camera problem

--- First_Game/Assets/Scripts/MovementInput.cs
+++ First_Game/Assets/Scripts/MovementInput.cs
@@ -25,6 +25,7 @@ public class MovementInput : MonoBehaviourPunCallbacks
     public float groundCheckDistance = 0.4f;
     public LayerMask groundMask;
     public float JumpHeight = 2f;
+    public float MoveSpeed = 2f;
 
     private void Start()
     {
@@ -35,7 +36,6 @@ public class MovementInput : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-
         if (photonView.IsMine)
         {
             InputMagnitude();
@@ -57,7 +57,7 @@ public class MovementInput : MonoBehaviourPunCallbacks
 
             controller.Move(velocity * Time.deltaTime);
 
-            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * Speed * MoveSpeed);
         }
     }
 
@@ -96,8 +96,9 @@ public class MovementInput : MonoBehaviourPunCallbacks
             //Calculate the Input Magnitude
             Speed = new Vector2(InputX, InputZ).normalized.sqrMagnitude;
 
-            //Physically move player
-            if (Speed > allowPlayerRotation)
+
+        //Physically move player
+        if (Speed > allowPlayerRotation)
             {
             anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
             PlayerMoveAndRotation();
