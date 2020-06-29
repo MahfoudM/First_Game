59186e0f51f7cd8d9835a1e5e0cbf4ffa49d45af
MahfoudM <m.mahfoud707@gmail.com>
2020-06-29 19:03:11 +0300

    New Unity Project (no multiplayer)

---
+++ FirstGame/Assets/Scripts/BillBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCameraTransform.rotation * Vector3.forward, mainCameraTransform.rotation * Vector3.up);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ FirstGame/Assets/Scripts/MovementInput.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+    public float InputX;
+    public float InputZ;
+    public Vector3 desiredMoveDirection;
+    public bool blockRotationPlayer;
+    public float desiredRotationSpeed = 0.1f;
+    public Animator anim;
+    public float Speed;
+    public float allowPlayerRotation;
+    public Camera cam;
+    public CharacterController controller;
+    public bool isGrounded;
+    private float verticalVel;
+    private Vector3 moveVector;
+    public float gravity = -9;
+    Vector3 velocity;
+    public Transform groundCheck;
+    public float groundCheckDistance = 0.4f;
+    public LayerMask groundMask;
+    public float JumpHeight = 2f;
+    public float MoveSpeed = 0f;
+    private bool Run;
+    private bool Jump;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        controller = this.GetComponent<CharacterController>();
+        anim = this.GetComponent<Animator>();
+        Run = false;
+    }
+
+    private void Update()
+    {
+            InputMagnitude();
+
+            checkShift();
+            checkSpeed();
+            checkJump();
+
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Jump && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(JumpHeight * -2f * gravity);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    anim.Play("Jump");
+                }
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+
+            anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+
+            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
+    }
+
+    void PlayerMoveAndRotation()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        var camera = Camera.main;
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * InputZ + right * InputX;
+
+        if (blockRotationPlayer == false)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
+        }
+    }
+
+    void InputMagnitude()
+    {
+        //Calculate Input Vectors
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        anim.SetFloat("InputZ", InputZ, 0.0f, Time.deltaTime * 2f);
+        anim.SetFloat("InputX", InputX, 0.0f, Time.deltaTime * 2f);
+
+        //Calculate the Input Magnitude
+        Speed = new Vector2(InputX, InputZ).normalized.sqrMagnitude;
+
+
+        //Physically move player
+        if (Speed > allowPlayerRotation)
+        {
+            anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+            PlayerMoveAndRotation();
+        }
+        else if (Speed < allowPlayerRotation)
+        {
+            anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+        }
+    }
+
+    void checkSpeed()
+    {
+        if (Speed <= 1 && Speed >= 0.5 && !Run)
+        {
+            MoveSpeed = 2f;
+        }
+        else if (Speed >= 0 && Speed <= 0.2)
+        {
+            MoveSpeed = 0f;
+        }
+        else if (Speed <= 1 && Speed >= 0.5 && Run)
+        {
+            MoveSpeed = 4f;
+        }
+    }
+
+    void checkShift()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Run = true;
+        }
+        else
+        {
+            Run = false;
+        }
+        anim.SetBool("Run", Run);
+    }
+
+    void checkJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump = true;
+        }
+        else if (isGrounded && Input.GetKey(KeyCode.Space) == false)
+        {
+            Jump = false;
+        }
+    }
+}
