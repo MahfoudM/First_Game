15db5734ee073cbc65a29e8f34397dc691c8b150
MahfoudM <m.mahfoud707@gmail.com>
2020-07-06 22:40:31 +0300

    i dont know

---
+++ FirstGame/Assets/Scripts/MapCameraScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCameraScript : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+
+    public float scrollSpeed = 20f;
+    public float minY = 20f;
+    public float maxY = 120f;
+    
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.mousePosition.y >= 1030 - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= 70 - panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= 1450 - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= 480 - panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * 100f * scrollSpeed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);    
+
+        transform.position = pos;
+    }
+}
---
+++ FirstGame/Assets/Scripts/MapScript.cs
@@ -0,0 +1,61 @@
+﻿using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScript : MonoBehaviour
+{
+    public static bool MapIsOn = false;
+
+    public GameObject MapUI;
+    public GameObject GameplayUI;
+    public GameObject Player;
+    public GameObject CinemachineCamera;
+    public MovementInput MovementScript;
+
+    private void Update()
+    {
+       // updatePlayerPosition();
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (MapIsOn)
+            {
+                closeMap();   
+            }
+            else
+            {
+                openMap();
+            }
+        }
+    }
+
+    private void openMap()
+    {
+        MapUI.SetActive(true);
+        GameplayUI.SetActive(false);
+        MapIsOn = true;
+       // Time.timeScale = 0f;
+        CinemachineCamera.SetActive(false);
+        MovementScript.DisableMovement = true;
+        MovementScript.blockRotationPlayer = true;
+    }
+
+    private void closeMap()
+    {
+        MapUI.SetActive(false);
+        GameplayUI.SetActive(true);
+        MapIsOn = false;
+       // Time.timeScale = 1f;
+        CinemachineCamera.SetActive(true);
+        MovementScript.DisableMovement = false;
+        MovementScript.blockRotationPlayer = false;
+    }
+    /*
+        void updatePlayerPosition()
+        {
+            PlayerIndicator.transform.position = Player.transform.position; // + some tuning
+        }
+     */
+}
