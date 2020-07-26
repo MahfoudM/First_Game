72fd86bd710f0f135a1b55268881036ced9d0404
MahfoudM <m.mahfoud707@gmail.com>
2020-07-26 14:03:24 +0300

    idk

---
+++ FirstGame/Assets/Scripts/NPCRandomDestination.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCRandomDestination : MonoBehaviour
+{
+    public int genPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "NPC")
+        {
+            genPos = Random.Range(1, 5);
+            if (genPos == 4)
+            {
+                this.gameObject.transform.position = new Vector3(5, 1, -71);
+            }
+            if (genPos == 3)
+            {
+                this.gameObject.transform.position = new Vector3(145.8f, 1, -70);
+            }
+            if (genPos == 2)
+            {
+                this.gameObject.transform.position = new Vector3(145.8f, 1, 69);
+            }
+            if (genPos == 1)
+            {
+                this.gameObject.transform.position = new Vector3(5.83f, 1, 69);
+            }
+        }
+    }
+}
